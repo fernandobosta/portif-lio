91baefe744f310eae12e012d9003cbd65808be5d
fernandobosta <fernando.kanha@escola.pr.gov.br>
2025-05-19 08:55:47 -0300

    Add files via upload

---
+++ exercicio3.js
@@ -0,0 +1,46 @@
+
+let num1;
+let num2;
+let resultado;
+let nu;
+
+function sobra(){
+
+    num1 = Number(window.prompt('Digite um número'));
+    num2 = Number(window.prompt('Digite outro número'));
+
+    resultado = num1 % num2;
+
+    alert("O resultado é " + resultado);
+
+}
+
+function multiplicacao(){
+
+    num1 = Number(window.prompt('Digite um número'));
+    num2 = Number(window.prompt('Digite outro número'));
+
+    resultado = num1 * num2;
+
+    alert("O resultado é " + resultado);
+}
+
+function adicao(){
+
+    num1 = Number(window.prompt('Digite um número'));
+    num2 = Number(window.prompt('Digite outro número'));
+
+    resultado = num1 + num2;
+
+    alert("O resultado é " + resultado);
+}
+
+function subtracao(){
+
+    num1 = Number(window.prompt('Digite um número'));
+    num2 = Number(window.prompt('Digite outro número'));
+
+    resultado = num1 - num2;
+
+    alert("O resultado é " + resultado);
+}
